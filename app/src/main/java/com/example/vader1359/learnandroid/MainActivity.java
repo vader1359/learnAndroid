953f0c2827ce86bf99a3be10013a7c14dad26de4
vader1359 <vader1359@gmail.com>
2017-05-29 17:12:31 +0700

    Create learnAndroid project

---
+++ app/src/main/java/com/example/vader1359/learnandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vader1359.learnandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
